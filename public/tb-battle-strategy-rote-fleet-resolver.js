ac68666de9d4fdddd874e125986aa3f426c537f5
366 Industries <103216816+Hunting-Fishing@users.noreply.github.com>
2026-08-16 12:50:32 +0800

    Canonicalize required ROTE fleet ship identifiers

---
+++ public/tb-battle-strategy-rote-fleet-resolver.js
@@ -0,0 +1,24 @@
+import { roteFleetBattleStrategyForMission as rawFleetStrategy } from "./tb-battle-strategy-rote-fleet-data.js";
+
+const SHIP_BASE_IDS = Object.freeze({
+  "Lando's Millennium Falcon": "MILLENNIUMFALCONPRISTINE",
+  Outrider: "OUTRIDER",
+  Executor: "CAPITALEXECUTOR",
+  Profundity: "CAPITALPROFUNDITY",
+  Negotiator: "CAPITALNEGOTIATOR",
+  Ghost: "GHOST",
+  "Gauntlet Starfighter": "GAUNTLETSTARFIGHTER",
+  "Imperial TIE Fighter": "TIEFIGHTERIMPERIAL",
+});
+
+export function roteFleetBattleStrategyForMission(missionId) {
+  const strategy = rawFleetStrategy(missionId);
+  if (!strategy) return null;
+  return {
+    ...strategy,
+    keyUnits: (strategy.keyUnits || []).map((unit) => ({
+      ...unit,
+      baseId: SHIP_BASE_IDS[unit.name] || String(unit.baseId || ""),
+    })),
+  };
+}
